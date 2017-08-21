9a29a27a8eceb814e71a774e433d4e9677bbe34e
maynor1993 <31224909+maynor1993@users.noreply.github.com>
2017-08-21 17:24:10 -0600

    Primer Commit
    Creación del Proyecto

---
+++ buscaMinas_A201413473/src/practica1_201413473/practica1_201413473.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_201413473;
+
+/**
+ *
+ * @author Maynor Garcia 201413473
+ */
+public class practica1_201413473 {
+    
+}
